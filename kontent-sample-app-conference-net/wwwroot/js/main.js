676a8a003f74ed7861d6533857ab2b24775c1600
MichaelB <MichaelB@kentico.com>
2020-02-25 07:56:24 -0500

    removed unused CSS and JavaScript

--- kontent-sample-app-conference-net/wwwroot/js/main.js
+++ kontent-sample-app-conference-net/wwwroot/js/main.js
@@ -44,14 +44,6 @@ $(window).scroll(function () {
   // Initiate the wowjs animation library
   new WOW().init();
 
-  // Initialize Venobox
-  $('.venobox').venobox({
-    bgcolor: '',
-    overlayColor: 'rgba(6, 12, 34, 0.85)',
-    closeBackground: '',
-    closeColor: '#fff'
-  });
-
   // Initiate superfish on nav menu
   $('.nav-menu').superfish({
     animation: {
@@ -133,25 +125,5 @@ $(window).scroll(function () {
       }
     }
   });
-
-  // Gallery carousel (uses the Owl Carousel library)
-  $(".gallery-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    center:true,
-    responsive: { 0: { items: 1 }, 768: { items: 3 }, 992: { items: 4 }, 1200: {items: 5}
-    }
-  });
-
-  // Buy tickets select the ticket type on click
-  $('#buy-ticket-modal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget);
-    var ticketType = button.data('ticket-type');
-    var modal = $(this);
-    modal.find('#ticket-type').val(ticketType);
-  })
-
-// custom code
 
 });
